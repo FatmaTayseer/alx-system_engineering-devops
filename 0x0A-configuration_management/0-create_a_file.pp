514ed68e9da0025eccb995bbfab432a92023dcc7
Fatma Tayseer <127580246+FatmaTayseer@users.noreply.github.com>
2023-10-28 00:16:40 +0200

    update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+0-create_a_file.pp#Create a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
